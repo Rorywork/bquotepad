dcc958cc54d5357b8a7d9316cb1a8dc1cefe9160
Rorywork <rorywork95@outlook.com>
2019-10-13 14:40:24 +0100

    Removed comments from views.py within payments folder

--- src/payments/views.py
+++ src/payments/views.py
@@ -11,7 +11,7 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 class HomePageView(TemplateView):
     template_name = 'payments/stripe_checkout.html'
 
-    def get_context_data(self, **kwargs): # new
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['key'] = settings.STRIPE_PUBLISHABLE_KEY
         return context
