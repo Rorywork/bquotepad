797f51477dbef56980eff53d896e7a68177cd218
Rorywork <rorywork95@outlook.com>
2019-10-13 14:16:02 +0100

    Removed comments from urls.py

--- src/bquotepad/urls.py
+++ src/bquotepad/urls.py
@@ -20,30 +20,22 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from quotepad.views import home, register, change_password, dashboard
+from quotepad.views import home, register, change_password, landing
 from quotepad.forms import FormStepOne, FormStepTwo, FormStepThree, FormStepFour, FormStepFive, FormStepSix, FormStepSeven, FormStepEight, FormStepNine
 from quotepad.views import FormWizardView, model_form_upload
 
+from quotepad.views import edit_Profile_details, show_uploaded_files, quote_generated, quote_emailed, quote_not_possible, quotepad_template_help
 from quotepad.views import ProductPriceList, ProductPriceCreate, ProductPriceUpdate, ProductPriceDelete
-
-from quotepad.views import edit_Profile_details, show_uploaded_files, quote_success, testpdflayout
+from quotepad.views import generate_quote_from_file, edit_quote_template, list_quote_archive, pdf_view
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^boilerform/$', FormWizardView.as_view([FormStepOne,FormStepTwo])),
-    #url(r'^$', home),
-    #url(r'^register/', register),
-    # url(r'^login/$', auth_views.login),
-    #url(r'^logout/$', auth_views.LogoutView.as_view()),
     path('logout/', auth_views.LogoutView.as_view()),
-    #url(r'^changepassword/$', change_password, name = 'change_password'),
-    #path('', home_view, name='home'),
-    path('', home, name='home'),
+    path('', landing, name='landing'),
     path('login/', auth_views.LoginView.as_view()),
     path('passwordreset/', auth_views.PasswordResetView.as_view()),
     path('register/', register),
 
-    #path('accounts/', include('accounts.urls')),
     path('boilerinfo/', include('django.contrib.auth.urls')),
 
     path('productpricelist/', ProductPriceList.as_view(), name = 'productpricelist'),
@@ -51,18 +43,25 @@ urlpatterns = [
 	path('productpriceupdate/<int:product_id>/', ProductPriceUpdate, name = 'productpriceupdate'),
 	path('productpricedelete/<int:pk>/', ProductPriceDelete.as_view(), name = 'productpricedelete'),
 
-    # Below loginredirect/ is in settings.py
+    path('quotegenerated/', quote_generated, name = 'quote_generated'),
+	path('quoteemailed/', quote_emailed, name = 'quote_emailed'),
+    path('quotenotpossible/', quote_not_possible, name = 'quote_not_possible'),
+	path('quotepadtemplatehelp/', quotepad_template_help, name = 'quotepad_template_help'),
+
     path('loginredirect/', home, name = 'home'),
     path('changepassword/', change_password, name = 'change_password'),
     path('home/', home, name = 'home'),
-    path('dashboard/', dashboard, name = 'dashboard'),
+
+    path('landing/', landing, name = 'landing'),
     path('boilerform/', FormWizardView.as_view([FormStepOne,FormStepTwo,FormStepThree, FormStepFour, FormStepFive, FormStepSix, FormStepSeven, FormStepEight, FormStepNine]), name = 'boilerform'),
-    path('testpdflayout/<str:testmode>', testpdflayout, name = 'test_pdf_layout'),
-    #url(r'^', include('boilerform.urls'))
+    path('generatequotefromfile/<str:outputformat>/<str:quotesource>', generate_quote_from_file, name = 'generate_quote_from_file'),
+
     path('fileupload/', model_form_upload, name = 'file_upload'),
     path('showuploadedfiles/', show_uploaded_files, name = 'show_uploaded_files'),
-    path('quotesuccess/', quote_success, name = 'quote_success'),
-    #url(r'^pdf/$', GeneratePDF.as_view()),
-    # url(r'^pdf/$', GeneratePDF2, name = 'GenPDF'),
+
     path('edit_Profile_details/', edit_Profile_details, name = 'edit_Profile_details'),
+    path('editquotetemplate/', edit_quote_template, name = 'editquotetemplate'),
+	path('listquotearchive/', list_quote_archive, name = 'listquotearchive'),
+	path('pdfview/<str:pdf_file>', pdf_view, name = 'pdfview'),
+	path('', include('payments.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
