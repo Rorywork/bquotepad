bcd97c13582865b28df0405f861c7baad3114632
Rorywork <rorywork95@outlook.com>
2019-10-14 16:39:18 +0100

    Updated bug in utils, to allow for absolute path names

--- src/quotepad/admin.py
+++ src/quotepad/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Document, Profile
+from .models import Document, Profile, ProductPrice
 
 admin.site.register(Document)
 admin.site.register(Profile)
+admin.site.register(ProductPrice)
