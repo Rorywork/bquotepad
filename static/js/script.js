09a1f95638ff5bf462053b9e2e9e9a2fc5e656d9
Rorywork <rorywork95@outlook.com>
2019-10-16 14:53:13 +0100

    Created profile_form_validation_response function in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,12 @@ function all_good(headertext,maintext) {
 function not_all_good(headertext,maintext) {
     return Swal.fire(headertext, maintext, "error")
 }
+
+function profile_form_validation_response() {
+    if (document.ProfileForm.alert.value==1){       
+        all_good("All Good", "Your profile update has been saved")
+    }
+    if (document.ProfileForm.alert.value==0){
+        not_all_good("Oops!", "There was a problem with your profile update.")
+    }
+}
