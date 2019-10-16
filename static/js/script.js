944d4bc30725ff0b2944d4f5d6bfcd75c262b8a8
Rorywork <rorywork95@outlook.com>
2019-10-16 14:52:46 +0100

    Created not_all_good function in js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,7 @@
 function all_good(headertext,maintext) {
     return Swal.fire(headertext, maintext, "success")
 }
+
+function not_all_good(headertext,maintext) {
+    return Swal.fire(headertext, maintext, "error")
+}
